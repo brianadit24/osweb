8ac2cd37ae2d22788572ee2b64c69262fc8db434
brianadit24 <brian.adityaherman@gmail.com>
2019-01-19 14:48:21 +0700

    Project Web for OS

---
+++ assets/js/1.js
@@ -0,0 +1,22 @@
+function myFunction() {
+    var x = document.getElementById("navnav");
+    if (x.className === "nav") {
+        x.className += " responsive";
+    } else {
+        x.className = "nav";
+    }
+}
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
